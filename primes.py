b31c489d60f62d0be1bc8a9ac2b75dd31c1ef485
k22037403 <k22037403@kcl.ac.uk>
2023-09-26 21:09:36 +0100

    shiuld work

--- primes.py
+++ primes.py
@@ -2,23 +2,26 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    if number_of_primes <= 0:
-        raise ValueError
-    list = []
-    count = 0
-    while len(list) < number_of_primes:
-        if count == 0 or count == 1:
-            count += 1
-            continue
+    try:
+        if number_of_primes <= 0:
+            raise ValueError
         else:
-            for j in range(2, int(count/2) + 1):
-                if count % j == 0:
-                    count += 1
-                    break
-            else:
-                list.append(count)
-                count += 1
-    print(list)
-    return list
+            list = []
+            count = 0
+            while len(list) < number_of_primes:
+                if count == 0 or count == 1:
+                   count += 1
+                   continue
+                else:
+                    for j in range(2, int(count/2) + 1):
+                        if count % j == 0:
+                            count += 1
+                            break
+                    else:
+                        list.append(count)
+                        count += 1
+    except:
+        raise ValueError("AHhh")
+                    
 
-primes(0)
+primes(-1)
