bb35ba42563f14c83ee51b1ddae7b299d5418246
k22037403 <k22037403@kcl.ac.uk>
2023-09-27 13:22:47 +0100

    final

--- primes.py
+++ primes.py
@@ -1,27 +1,28 @@
+
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
     try:
         if number_of_primes <= 0:
-            raise ValueError
+                raise ValueError
         else:
-            list = []
-            count = 0
-            while len(list) < number_of_primes:
-                if count == 0 or count == 1:
-                   count += 1
-                   continue
-                else:
-                    for j in range(2, int(count/2) + 1):
-                        if count % j == 0:
-                            count += 1
-                            break
-                    else:
-                        list.append(count)
-                        count += 1
+            return createPrimes(number_of_primes)
     except:
-        raise ValueError("AHhh")
-                    
-
-primes(-1)
+        print(f"OOps we caught a Value Error ")
+        
+def createPrimes(number_of_primes):
+    list = []
+    count = 0
+    while len(list) < number_of_primes:
+        if count == 0 or count == 1:
+            count += 1
+            continue
+        else:
+            for j in range(2, int(count/2) + 1):
+                if count % j == 0:
+                    count += 1
+                    break
+            else:
+                list.append(count)
+                count += 1
+    return list
