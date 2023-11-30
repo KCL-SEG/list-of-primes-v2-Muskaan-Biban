67617757f3af7dbb062645c05594c9b097c624f3
Muskaan Biban (k22037403) <muskaan.biban@kcl.ac.uk>
2023-11-30 16:15:13 +0000

    finish primes

--- primes.py
+++ primes.py
@@ -2,31 +2,22 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def check_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num /2)):
+        if num % i == 0:
+            return False
+        
+    return True 
 def primes(number_of_primes):
-    try:
-        if number_of_primes <= 0:
-            raise ValueError("ValueError")
-        else:
-             return createPrimes(number_of_primes)
-    except ValueError as e:
-        print(f'We caught a {e.__class__}')
-        
-        
-def createPrimes(number_of_primes):
-    list = []
-    count = 0
-    while len(list) < number_of_primes:
-        if count == 0 or count == 1:
-            count += 1
-            continue
-        else:
-            for j in range(2, int(count/2) + 1):
-                if count % j == 0:
-                    count += 1
-                    break
-            else:
-                list.append(count)
-                count += 1
-    return list
-
-primes(-1)
+    arr = []
+    num = 2
+    if number_of_primes > 0:
+        while (len(arr) < number_of_primes):
+            if check_prime(num):
+                arr.append(num)
+            num = num + 1
+    else:
+        raise ValueError
+    return arr
